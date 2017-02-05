51a1905fc3a8eeb7ca66edd8ad1423f8c9a353fb
fSmooth <fSmoothdev@gmail.com>
2017-02-05 12:58:23 +0100

    Implementación de BarcodeTracker

---
+++ app/src/main/java/com/fsmooth/monocular/barcode/BarcodeTracker.java
@@ -0,0 +1,29 @@
+package com.fsmooth.monocular.barcode;
+
+import android.content.Context;
+
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+
+/**
+ * Created by fonsi on 5/02/17.
+ */
+
+public class BarcodeTracker extends Tracker<Barcode>{
+    private BarcodeGraphicTrackerCallback mListener;
+
+    public interface BarcodeGraphicTrackerCallback {
+        void onDetectedQrCode(Barcode barcode);
+    }
+
+    BarcodeTracker(Context listener) {
+        mListener = (BarcodeGraphicTrackerCallback) listener;
+    }
+
+    @Override
+    public void onNewItem(int id, Barcode item) {
+        if (item.displayValue != null) {
+            mListener.onDetectedQrCode(item);
+        }
+    }
+}
